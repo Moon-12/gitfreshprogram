c6e76294065983f63d7975b6fc3b8b2fcc8db1ab
Moon-12 <ashwijanayak@gmail.com>
2020-12-18 02:34:48 +0530

    Ashwi v1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 const results = [];
-
+console.log("Ashwi has added changes");
 fs.createReadStream("data1.csv")
   .pipe(csv())
   .on("data", (data) => results.push(data))
