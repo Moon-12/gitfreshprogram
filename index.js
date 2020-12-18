e841bbad36cea8e00a830bc2997964331c2bfee0
Moon-12 <66021345+Moon-12@users.noreply.github.com>
2020-12-18 16:23:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ fs.createReadStream("data1.csv")
   .on("data", (data) => results.push(data))
   .on("end", () => {
     console.log(results);
-    console.log("hello");
+    console.log("hello1");
   });
